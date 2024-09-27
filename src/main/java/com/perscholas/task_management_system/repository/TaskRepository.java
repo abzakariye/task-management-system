0c1c28678bf34a1bc01eac34db8e2fb5986f9143
abdiza01 <abdilezakariye@gmail.com>
2024-09-26 20:18:58 -0400

    Implemented Task Repository Layer

---
+++ src/main/java/com/perscholas/task_management_system/repository/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.perscholas.task_management_system.repository;
+
+import com.perscholas.task_management_system.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+}
